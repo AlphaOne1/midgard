8628055a68c01b9096483ddd2a2a2cb63cce0bea
Alexander Adam <alphaone23@gmail.com>
2024-06-13 20:10:59 +0200

    Added utility tests.

--- util/util.go
+++ util/util.go
@@ -4,15 +4,19 @@ import (
 	"log/slog"
 	"net/http"
 	"os"
+	"testing"
 )
 
 // Must exits the program if the given pair of function return and error contains an non-nil error value,
 // otherwise the function return value val is returned.
 func Must[T any](val T, err error) T {
 	if err != nil {
-		slog.Error("must condition not met",
+		slog.Error("must-condition not met",
 			slog.String("error", err.Error()))
-		os.Exit(1)
+
+		if !testing.Testing() {
+			os.Exit(1)
+		}
 	}
 
 	return val
---
+++ util/util_test.go
@@ -0,0 +1,51 @@
+package util
+
+import (
+	"bytes"
+	"errors"
+	"log/slog"
+	"net/http"
+	"net/http/httptest"
+	"regexp"
+	"testing"
+)
+
+func TestMustGood(t *testing.T) {
+	got := Must("pass", nil)
+
+	if got != "pass" {
+		t.Errorf(`expected "pass" but got %v`, got)
+	}
+}
+
+func TestMustBad(t *testing.T) {
+	outbuf := bytes.Buffer{}
+
+	slog.SetDefault(slog.New(slog.NewTextHandler(&outbuf, &slog.HandlerOptions{})))
+
+	got := Must("nopass", errors.New("testerror"))
+
+	if got != "nopass" {
+		t.Errorf("got %v but wanted `nopass`", got)
+	}
+
+	outputMatch := regexp.MustCompile(`^time=[^ ]+ level=ERROR msg="must-condition not met" error=testerror\n$`)
+
+	if !outputMatch.Match(outbuf.Bytes()) {
+		t.Errorf("output does not match format %v, got %v",
+			outputMatch.String(),
+			outbuf.String())
+	}
+}
+
+func TestDummyHandler(t *testing.T) {
+	req := httptest.NewRequest(http.MethodGet, "/", nil)
+	rec := httptest.NewRecorder()
+
+	DummyHandler(rec, req)
+
+	if rec.Body.String() != "dummy" {
+		t.Errorf("wanted Dummy but got %v",
+			slog.String("body", rec.Body.String()))
+	}
+}
